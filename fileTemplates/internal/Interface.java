412676aa7e08d3201a9195d62381d45f1c2dec94
radish <zhouxiangyang@dxy.cn>
2020-04-21 16:57:32 +0800

    IU-2018.1.7 <radish@hellodeMacBook-Pro.local Create markdown.xml, ide.general.xml, project.default.xml, equalsHashCodeTemplates.xml, Enum.java, Interface.java, lombok-plugin.xml, git.xml, other.xml, gradle.experimental.xml, Default.xml, find.xml, ui.lnf.xml, gradle.settings.xml, filetypes.xml, projectView.xml, gradle.run.settings.xml, plugin_ui.xml, colors.scheme.xml, method.xml, path.macros.xml, databaseDrivers.xml, github_settings.xml, mavenVersion.xml, templates.xml, AnnotationType.java, Default.xml, go.sdk.xml, Class.java, jdk.table.xml, debugger.renderers.xml, mavenRunHelper.xml, debugger.threadsview.xml, laf.xml, vcs.xml, editor.codeinsight.xml, databaseSettings.xml, toStringTemplates.xml, keymap.xml, editor.xml, usageView.xml, debugger.xml

---
+++ fileTemplates/internal/Interface.java
@@ -0,0 +1,9 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+ /**
+ * description 
+ * @author  zxy
+ * create time ${DATE} ${HOUR}:${HOUR}  
+ */
+public interface ${NAME} {
+}
