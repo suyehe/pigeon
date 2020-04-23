529f548042c9d98eba41325e5ac1f8ecca64897f
radish <zhouxiangyang@dxy.cn>
2020-04-23 10:45:41 +0800

    refact

--- fileTemplates/internal/AnnotationType.java
+++
@@ -1,9 +0,0 @@
-#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
-#parse("File Header.java")
- /**
- * description 
- * @author  zxy
- * @create time ${DATE} ${HOUR}:${HOUR}  
- */
-public @interface ${NAME} {
-}
